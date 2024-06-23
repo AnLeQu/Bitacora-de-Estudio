e9704b617d1d700df580edde7143d6f222491bdd
AnLeQu <anlequ@outlook.com>
2024-06-23 00:19:35 -0600

    prueba

--- FPP-ONEG7/1-Logica de programacion/Realidad y Avance/Hora de practicar/Desafio 1-10.js
+++ FPP-ONEG7/1-Logica de programacion/Realidad y Avance/Hora de practicar/Desafio 1-10.js
@@ -1,7 +1,7 @@
 // Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
 let edad = 18;
-let edaddecompr = prompt("Queremos confirmar tu edad para saber si puedes tener esta licencia. Edad:");
-console.log(Edad);
+let edaddecompr = prompt ("Queremos confirmar tu edad para saber si puedes tener esta licencia. Edad:");
+console.log(edaddecompr);
 
 if (edaddecompr == edad)
     {alert("¡Puedes obtener tu licencia de conducir!")}
