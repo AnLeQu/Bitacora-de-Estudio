1fa98d98914e2c26353209f38e6eb32cfbdd1406
AnLeQu <anlequ@outlook.com>
2024-06-23 19:41:38 -0600

    desafios

---
+++ FPP-ONEG7/1-Logica de programacion/Progreso/Hora de practicar/Desafios #2/D2-5.js
@@ -0,0 +1,6 @@
+//D2-5 Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
+
+//Variables
+let NomUsu = prompt("Cual es tu nombre: ") //Nombre de Usuario
+console.log(NomUsu)
+alert("Bienvenido(a) a nuestra pagina web " + NomUsu)
