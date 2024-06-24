744b9ee50ed59962ae8c1c4f9f11ffdbd2544f65
AnLeQu <anlequ@outlook.com>
2024-06-23 19:28:37 -0600

    Saldo

---
+++ FPP-ONEG7/1-Logica de programacion/Progreso/Hora de practicar/Desafios #2/D2-4.js
@@ -0,0 +1,6 @@
+//D2-4 Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+
+//Variables
+let N = 1000000;
+let tusaldo = "El saldo de tu cuenta es de: " + Math.floor(Math.random() * N);
+alert(tusaldo)
