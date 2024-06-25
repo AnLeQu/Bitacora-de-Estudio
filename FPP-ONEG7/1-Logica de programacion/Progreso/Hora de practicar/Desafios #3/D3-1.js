9a29d6a06a1187135109bd55f82c3020c02ea762
AnLeQu <133515787+AnLeQu@users.noreply.github.com>
2024-06-25 15:51:15 -0600

    desafio

--- FPP-ONEG7/1-Logica de programacion/Progreso/Hora de practicar/Desafios #3/D3-1.js
+++ FPP-ONEG7/1-Logica de programacion/Progreso/Hora de practicar/Desafios #3/D3-1.js
@@ -1,1 +1,7 @@
 //D3-1 Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+let contador = 1
+while (contador < 11)
+    {
+        alert(contador + 1)
+    }
+    
