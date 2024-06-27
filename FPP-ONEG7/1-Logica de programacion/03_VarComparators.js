aff7c8808992cd40aa9ad5c56938e984e5dc0d42
AnLeQu <anlequ@outlook.com>
2024-06-27 02:08:26 -0600

    update

--- FPP-ONEG7/1-Logica de programacion/03_VarComparators.js
+++ FPP-ONEG7/1-Logica de programacion/03_VarComparators.js
@@ -1,12 +1,12 @@
 //Este codigo realiza la comparación de variables
 /*
-//IF
+IF (parametro/condición)
     {
         Si la condición planteada es igual o almenos 
         entró entre los límites establecidos es verdadera
     }
 
-//ELSE IF 
+ELSE IF (nuevo parametro != al anterior)
     {
         Si la condición planteada es falsa y no entró 
         entre los límites establecidos, estas variables 
@@ -14,13 +14,13 @@
         cuales comparar con algun otro argumento.
     }
         
-//ELSE
+ELSE
     {
         Sino se le da alguna condicion en caso de no 
         entrar entre los parametros anteriores
     }
         
-//WHILE 
+WHILE (parametro)
     {
         Mientras alguna condicion establecida se de, 
         el codigo dentro de el seguira funcionando de
